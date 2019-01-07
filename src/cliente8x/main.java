ff19870ddd6fe9f86d662a1d9574b19adfb5c2e3
jonathansantana19 <jona_than101@msn.com>
2019-01-07 21:59:31 -0200

    first commit

---
+++ src/cliente8x/main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cliente8x;
+
+import org.apache.log4j.BasicConfigurator;
+
+/**
+ *
+ * 
+ */
+public class main {
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        FrmClient client = new FrmClient();
+    }
+
+}
+
+
+
+
